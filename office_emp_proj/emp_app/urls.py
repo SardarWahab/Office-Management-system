735551e4251aba4f9a8b0853caf692cdaaa82872
SardarWahab <wahabsardar130@gmail.com>
2024-11-04 17:02:45 +0500

    "Updated index.html to use named URL 'remove_emp1', changed URL pattern name in urls.py, and modified db.sqlite3 and __pycache__ files."

--- office_emp_proj/emp_app/urls.py
+++ office_emp_proj/emp_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('all_emp', all_emp, name='all_emp'),
     path('add_emp', add_emp, name='add_emp'),
-    path('remove_emp', remove_emp, name='remove_emp'),  # This handles the case when no emp_id is provided
+    path('remove_emp', remove_emp, name='remove_emp1'),  # This handles the case when no emp_id is provided
     path('remove_emp/<int:emp_id>', remove_emp, name='remove_emp'),  # This handles the case when emp_id is provided
     path('filter_emp', filter_emp, name='filter_emp'),
 ]
